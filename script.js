d8956a7afff38fe5c4f894a2e114d83d85440707
Animesh <animesh080808@gmail.com>
2022-10-31 19:43:55 +0530

    Udpated the html & css

---
+++ script.js
@@ -0,0 +1,30 @@
+const container = document.getElementById('container')
+const colors = [  '#FF2D00', '#FFFF00', '3DFF00' , '#00FFFA' , '#004CFF' , '#4C00FF' , '#FF00B9' , '#FF0000']
+const SQUARES = 1000
+
+for( let i = 0 ; i < SQUARES ; i++){
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover' , () => setColor(square))
+    square.addEventListener('mouseover' , () => removeColor(square))
+
+    container.appendChild(square)
+}
+
+function setColor(element){
+   const color = getRandomColor()
+   element.style.background = color
+   element.style.boxShadow = `0 0 2px ${color} , 0 0 10px ${color}`
+
+}
+
+
+function removeColor(element){
+    element.style.background = '#1d1d1d'
+    element.style.boxShadow = '0 0 2px #000'
+}
+
+function getRandomColor(){
+    return colors[Math.floor(Math.random() * colors.length)]
+}
